e688b7ade2857398ee9e50258e92a03e84189e9b
onubunu <onubunu@hotmail.com>
2015-01-04 00:33:20 +0100

    IZIN

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,35 +1,51 @@
 class Permission < Struct.new(:employee, :customer)
   def allow?(controller, action)
-    #####
+    ##############
+  # Zugriff auf Customer-Startseite erlauben:
     return true if controller == "dashboard_customer"
-    return true if controller == "customers/registrations" && action.in?(%w[ new create ])
+  # Zugriff auf Devise-Customer-Registration anlegen erlauben, wenn kein Employee vorhanden:
+    return true if controller == "customers/registrations" && action.in?(%w[ new create ]) if !employee
+  # Zugriff auf Devise-Customer-Session anlegen/löschen erlauben:
     return true if controller == "customers/sessions" && action.in?(%w[ new create destroy ])   
+  # Zugriff auf Devise-Customer-Password-Vergessen-Funktion erlauben:
     return true if controller == "customers/passwords" 
+  # Zugriff auf Devise-Customer-Password-Nicht-Erhalten-Funktion erlauben:
     return true if controller == "customers/confirmations" 
+  # Zugriff auf Employee-Startseite erlauben:
     return true if controller == "dashboard_employee"
+  # Zugriff auf Devise-Employee-Session anlegen erlauben (nur wenn min. 1 Employee vorhanden):    
     return true if controller == "employees/sessions" && action.in?(%w[ new create destroy ]) && Employee.count >= 1
+  # Zugriff auf Devise-Employee-Registration anlegen erlauben (nur wenn kein Employee vorhanden):
     return true if controller == "employees/registrations" && action.in?(%w[ new create ]) && Employee.count == 0
+  # Zugriff auf Devise-Employee-Password-Vergessen-Funktion erlauben:
     return true if controller == "employees/passwords" 
+  # Zugriff auf Devise-Employee-Password-Nicht-Erhalten-Funktion erlauben (nur für den ersten Employee):
     return true if controller == "employees/confirmations" && Employee.count == 1
-    return true if employee.admin?
+  # Employee-Admin hat die selben Rechte
+    return true if employee && employee.admin?
 
-    #####
+    ##############
     if customer
+    # Zugriff auf Devise-Customer-Registration bearbeiten erlauben (nur wenn Customer vorhanden ist und kein Employee vorhanden ist):
       return true if controller == "customers/registrations" && action.in?(%w[ edit update ]) if !employee
-      return true if employee.admin?
+    # Employee-Admin hat die selben Rechte
+      return true if employee && employee.admin?
     end
 
-    #####
+    ##############
     if employee.manager?
-      return true if controller == "customers/registrations" && action.in?(%w[ edit update ]) if !employee
+    # Zugriff auf Devise-Customer-Registration anlegen/bearbeiten erlauben:
+      return true if controller == "customers/registrations" && action.in?(%w[ new create edit update ])
 
-      return true if controller == "employees" && action.in?(%w[ index show ])
+      #return true if controller == "employees" && action.in?(%w[ index show ])
 
+    # Zugriff auf Devise-Employee-Registration anlegen/bearbeiten erlauben:
       return true if controller == "employees/registrations" && action.in?(%w[ new create edit update ]) 
+    # Employee-Admin hat die selben Rechte
       return true if employee.admin?
     end
     
-    #####
+    ##############
     if employee.admin?
       return true if controller == "customers" && action.in?(%w[ show index edit update new create destroy ])
       return true if controller == "employees" && action.in?(%w[ show index edit update new create destroy ])
